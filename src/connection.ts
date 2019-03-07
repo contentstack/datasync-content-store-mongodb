8e70f57c76a6c2d3e038f718cd1b9fe1036a5018
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-03-07 13:10:07 +0530

    indexes bug resolved, will run in bkground

--- src/connection.ts
+++ src/connection.ts
@@ -4,11 +4,13 @@
 * MIT Licensed
 */
 
+import Debug from 'debug'
 import { isEmpty, isPlainObject, merge } from 'lodash'
 import { Db, MongoClient } from 'mongodb'
 import { logger } from './util/logger'
 import { validateMongodbConfig } from './util/validations'
 
+const debug = Debug('connection')
 interface IMongo {
   db: Db,
   client: any
@@ -40,7 +42,13 @@ export const connect = (config) => {
       validateMongodbConfig(mongoConfig)
       const connectionUri = mongoConfig.url || mongoConfig.uri
       const dbName = mongoConfig.dbName
+      const collectionName = mongoConfig.collectionName
       const options = mongoConfig.options
+
+      debug('connection url', connectionUri)
+      debug('db name', dbName)
+      debug('collection name', collectionName)
+      debug('db options', JSON.stringify(options))
 
       if (mongoConfig.indexes && isPlainObject(mongoConfig.indexes) && !(isEmpty(mongoConfig.indexes))) {
         indexes = merge(indexes, mongoConfig.indexes)
@@ -50,16 +58,41 @@ export const connect = (config) => {
 
       return client.connect().then(() => {
         instance.db = client.db(dbName)
+        logger.info(`Mongodb connection to ${connectionUri} established successfully!`)
 
-        instance.db.collection('contents').createIndex(indexes).then(() => {
-          logger.info(`Mongodb connection to ${connectionUri} established successfully!`)
-          instance.client = client
+        resolve(instance)
 
-          return resolve(instance)
-        }).catch(reject)
+        // Create indexes in the background
+        const bucket: any = []
+        for (let index in indexes) {
+          if (indexes[index] === 1 || indexes[index] === -1) {
+            bucket.push(createIndexes(collectionName, index, indexes[index]))
+          }
+        }
+
+        Promise.all(bucket)
+          .then(() => {
+            logger.info(`Indexes created successfully in ${collectionName}`)    
+          })
+          .catch((error) => {
+            logger.error(`Failed while creating indexes in ${collectionName}`)
+            logger.error(error)
+          })
       }).catch(reject)
     } catch (error) {
       return reject(error)
     }
   })
+}
+
+const createIndexes = (collectionId, index, type) => {
+  return instance.db.collection(collectionId)
+    .createIndex({
+      [index]: type
+    })
+    .then((result) => {
+      debug(`Indexes result for ${index}: ${JSON.stringify(result)}`)
+      logger.info(`Index created in collection: ${collectionId}, index-field: ${index}, index-type: ${type}`)
+      return
+    })
 }
