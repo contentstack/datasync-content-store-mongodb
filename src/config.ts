56be5b184363f84aa9c19dfc3f44572711892bea
Abhinav Gupta <abhinav.gupta@contentstack.com>
2022-11-09 21:05:11 +0530

    fix: updated version and added fix

--- src/config.ts
+++ src/config.ts
@@ -32,12 +32,12 @@ export const config = {
     dbName: 'contentstack-db',
     // http://mongodb.github.io/node-mongodb-native/3.1/api/MongoClient.html
     options: {
-      autoReconnect: true,
+      // autoReconnect: true,
       connectTimeoutMS: 15000,
       keepAlive: true,
       noDelay: true,
-      reconnectInterval: 1000,
-      reconnectTries: 20,
+      // reconnectInterval: 1000,
+      // reconnectTries: 20,
       useNewUrlParser: true,
     },
     unwantedKeys: {
